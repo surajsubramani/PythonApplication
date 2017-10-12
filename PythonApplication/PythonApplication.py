00ccf2968444b773059bbde5ba2bf457f6fb1bbc
surasubr <suraj.subramani-vineet@capgemini.com>
2017-10-12 09:48:37 -0400

    Test

--- PythonApplication/PythonApplication.py
+++ PythonApplication/PythonApplication.py
@@ -1,1 +1,9 @@
-print "hello - test"
+
+def some_func():
+    print 'in test 1, unproductive'
+    cup = 'coffee'
+
+if __name__ == '__main__':
+    # PythonApplication.py executed as script
+    # do something
+    some_func()
