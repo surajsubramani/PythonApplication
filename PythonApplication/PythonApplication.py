20e608ccbe3b401180d8ba50a8d87154a266da42
surasubr <suraj.subramani-vineet@capgemini.com>
2017-10-14 18:26:08 -0400

    ew

--- PythonApplication/PythonApplication.py
+++ PythonApplication/PythonApplication.py
@@ -1,7 +1,7 @@
 
 def some_func():
     print 'in APPLICATION_11 K1, unproductive'
-    cup = 'coffee'
+    cup = 'coffe2'
 
 if __name__ == '__main__':
     # PythonApplication.py executed as script1
