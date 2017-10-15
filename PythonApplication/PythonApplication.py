1c6aba9e3f9b2e5a5acdb08cf05cfba0dff76ef9
surasubr <suraj.subramani-vineet@capgemini.com>
2017-10-15 16:19:33 -0400

    Test

--- PythonApplication/PythonApplication.py
+++ PythonApplication/PythonApplication.py
@@ -9,7 +9,7 @@ def subtract(n1,n2):
 
 if __name__ == '__main__':
     # PythonApplication.py executed as script1
-    # do something
+    # do somethi1ng
     sum = add(1,2)
     difference = subtract(5,3)
 
