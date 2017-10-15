1a872767b6eca0d90ab81d297d7c986eaec9909f
surajsubramani <suraj.subramani-vineet@capgemini.com>
2017-10-15 11:53:38 -0400

    Update merge.bat

--- merge.bat
+++ merge.bat
@@ -1,4 +1,6 @@
+git remote -v
 git remote set-url origin git@github.com:surajsubramani/PythonApplication.git
+git remote -v
 git checkout Production
 git merge master
 git push --set-upstream origin Production
