18862cd2956900581aab709dd80accb76530ffc8
surasubr <suraj.subramani-vineet@capgemini.com>
2017-10-15 14:58:11 -0400

    Test

--- PythonApplication/PythonApplication.py
+++ PythonApplication/PythonApplication.py
@@ -1,15 +1,15 @@
 
-def calculate(n1,n2):
+def add(n1,n2):
     sum = n1+n2
     return sum
 
-def difference(n1,n2):
+def subtract(n1,n2):
     diff = n1-n2
     return diff
 
 if __name__ == '__main__':
     # PythonApplication.py executed as script1
     # do something
-    sum = sum(1,2)
-    difference = difference(5,3)
+    sum = add(1,2)
+    difference = subtract(5,3)
 
